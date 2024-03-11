0e554350e85f8e2390cfa4f060d39510683769c2
bngreen <5267104+bngreen@users.noreply.github.com>
2024-03-11 17:50:38 -0300

    update mod

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,10 +1,6 @@
-pub mod inscopemetrics {
-    pub mod kairosdb {
-        pub mod protos {
-            include!(concat!(
-                env!("OUT_DIR"),
-                "/io.inscopemetrics.kairosdb.proto.v2.rs"
-            ));
-        }
-    }
+pub mod protos {
+    include!(concat!(
+        env!("OUT_DIR"),
+        "/io.inscopemetrics.kairosdb.proto.v2.rs"
+    ));
 }
